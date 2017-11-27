c4af7ea2300e719064b4083b2da00047b5a76959
Daniel González <>
2017-11-27 22:36:38 +0100

    Commit inicial

---
+++ src/main/java/logger/LoggerDemo.java
@@ -0,0 +1,24 @@
+package logger;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Created by danielgonzalez on 26/11/17.
+ */
+public class LoggerDemo {
+
+    public void logs() {
+        Logger logger = LogManager.getLogger(this.getClass().getName());
+        logger.fatal("Log de fatal");
+        logger.error("Log de error");
+        logger.warn("Log de warning");
+        logger.info("Log de info");
+        logger.debug("Log de debug");
+        logger.trace("Log de trace");
+    }
+
+    public static void main(String[] args) {
+        new LoggerDemo().logs();
+    }
+}
